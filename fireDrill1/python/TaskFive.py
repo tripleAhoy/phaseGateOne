9a72f55ba8e71b268ec0d46547415faa2dd75036
Olawale Adegbite (tripleAhoy) <109899370+tripleAhoy@users.noreply.github.com>
2025-06-17 17:27:26 +0100

    Add files via upload

---
+++ fireDrill1/python/TaskFive.py
@@ -0,0 +1,27 @@
+public class TaskSix{
+
+	public static void main(String[] args) {
+		for(int count = 0; count < 5; count++) {
+			for( int counter = 1; counter < 11; counter++) {
+				if(counter % 4 == 0) {
+					System.out.print(counter);
+				}
+			}
+			
+		}
+	}
+}
+
+
+
+/*for(let count = 0; count < 5; count++) {
+	for(let count = 1; count < 10; count++) {
+		if(count % 4 == 0) {
+			console.log(count);
+		}
+	}
+
+}*/
+
+for number in range(4, 10, 4):
+	for numbers in 
---
+++ fireDrill1/python/TaskFour.py
@@ -0,0 +1,3 @@
+for number in range(1, 10):
+	if number % 4 == 0:
+		print(number)
