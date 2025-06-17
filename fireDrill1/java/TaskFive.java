97d22198e80aa215a2e564ef66b81da4d10f584e
Olawale Adegbite (tripleAhoy) <109899370+tripleAhoy@users.noreply.github.com>
2025-06-17 17:25:15 +0100

    Add files via upload

---
+++ fireDrill1/java/TaskFive.java
@@ -0,0 +1,24 @@
+public class TaskFive{
+
+	public static void main(String[] args) {
+		for(int count = 0; count < 5; count++) {
+			for( int counter = 1; counter < 11; counter++) {
+				if(counter % 4 == 0) {
+					System.out.print(counter);
+				}
+			}
+			
+		}
+	}
+}
+
+
+
+/*for(let count = 0; count < 5; count++) {
+	for(let count = 1; count < 10; count++) {
+		if(count % 4 == 0) {
+			console.log(count);
+		}
+	}
+
+}*/
---
+++ fireDrill1/java/TaskOne.java
@@ -0,0 +1,8 @@
+public class TaskOne{
+
+	public static void main(String[] args) {
+		for(int counter = 1; counter < 11; counter++) {
+			System.out.printf("%d%n", counter);
+		}
+	}
+}
